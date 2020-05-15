eb5552a61984098a929efc654cb2a749a836e53b
Elton Moraes <eltonsimor@gmail.com>
2020-05-14 21:02:33 -0300

    Partes finais de diretivas

---
+++ diretivas/src/app/shared/fundo-amarelo.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: 'p[fundoAmarelo]'
+})
+export class FundoAmareloDirective {
+
+  constructor(
+    private _elementRef: ElementRef,
+    private _renderer: Renderer2
+    ) {
+    this._renderer.setStyle(this._elementRef.nativeElement, 'background-color', 'yellow');
+  }
+
+}
